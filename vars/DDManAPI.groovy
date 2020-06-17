fcf2f37d9f5189c1a1f932c517bf3957454ab6db
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-06-18 01:26:19 +0200

    update

--- vars/DDManAPI.groovy
+++ vars/DDManAPI.groovy
@@ -44,7 +44,7 @@ def GetData() {
     //return ("${Prj}, ${VZ}, ${PK}")
     //return DDManPrjVzPk
     if (Prj!=''&& VZ!=''&& PK!=''&& DDManJob!=''){
-        execute( 'sh', "$(JavaPath) \$(JavaArchive $(JavaMemory) $(DDManPath) $(DDManModus[1]) $(DDManJob) -PRJ $(Prj) -SGP $(VZ) -PRG $(PK) -DB $(ddpar)")
+        execute( 'sh', "$(JavaPath) $(JavaArchive $(JavaMemory) $(DDManPath) $(DDManModus[1]) $(DDManJob) -PRJ $(Prj) -SGP $(VZ) -PRG $(PK) -DB $(ddpar)")
         //return ("${Prj}, ${VZ}, ${PK}")
 
     }
