a062cba6f4741b53817c2acacdedd3ab09c409a0
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-06-29 02:20:55 +0200

    2706

--- vars/DDManAPI.groovy
+++ vars/DDManAPI.groovy
@@ -50,15 +50,15 @@ def GetData() {
         //println(Command)
         //def sout = new StringBuilder()
         //def serr = new StringBuilder()
-        def DDManexecute= DDManCommand.execute()
-        String test=DDManexecute.text
+        //def DDManexecute= DDManCommand.execute()
+        //String test=DDManexecute.text
         //DDManexecute.consumeProcessOutput(System.out, System.err)
         //DDManexecute.waitFor()
         //Commandexecute.waitFor()
         //def args = ['cmd', '/c', 'C:\\Users\\AAithal\\Desktop\\MIR3\\bin\\inConsole', '-H', 'company.mir3.com', '-u', 'user', '-p', 'password', '-I', '-i', 'Server']
         //def proc = new ProcessBuilder( args )
         //Process process = proc.start()
-        return ("${test}")
+        return ("${DDManCommand}")
         //return ("${Command}")
         //return ("${Prj}, ${VZ}, ${PK}")
 
