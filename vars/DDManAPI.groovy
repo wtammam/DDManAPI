e5cf645faf788ced84812640eeac564a284ec2d9
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-06-29 02:54:10 +0200

    test

--- vars/DDManAPI.groovy
+++ vars/DDManAPI.groovy
@@ -46,11 +46,11 @@ def GetData() {
     if (Prj!=''&& VZ!=''&& PK!=''&& DDManJob!=''){
         //execute( 'bat', 'echo ok')
         //def x= "cmd /c ${JavaPath} ${JavaArchive} ${JavaMemory} ${DDManPath} ${DDManModus[1]} ${DDManJob} ${Prj} ${VZ} ${PK} ${DDPar}".execute().text
-        def DDManCommand= "\"${JavaPath}\" ${JavaArchive} ${JavaMemory} ${DDManPath} ${DDManModus[0]} ${DDManJob} PRJ=${Prj} PS=${VZ} PK=${PK} DB=${DDPar}"
+        //def DDManCommand= "\"${JavaPath}\" ${JavaArchive} ${JavaMemory} ${DDManPath} ${DDManModus[0]} ${DDManJob} PRJ=${Prj} PS=${VZ} PK=${PK} DB=${DDPar}"
         //println(Command)
         //def sout = new StringBuilder()
         //def serr = new StringBuilder()
-        def DDManexecute= DDManCommand.execute()
+        def DDManexecute= "\"${JavaPath}\" ${JavaArchive} ${JavaMemory} ${DDManPath} ${DDManModus[0]} ${DDManJob} PRJ=${Prj} PS=${VZ} PK=${PK} DB=${DDPar}".execute()
         def test3=DDManexecute.text
         //DDManexecute.consumeProcessOutput(System.out, System.err)
         //DDManexecute.waitFor()
