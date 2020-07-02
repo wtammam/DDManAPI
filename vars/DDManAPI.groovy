a35deba952831083d76355b669c12e625d3a0014
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-07-03 01:55:13 +0200

    0207

--- vars/DDManAPI.groovy
+++ vars/DDManAPI.groovy
@@ -3,7 +3,8 @@
 //package vars
 class DDManAPI {
 
-    private String JavaPath = "C:\\Program Files (x86)\\Java\\jre1.8.0_251\\bin\\java.exe"
+    //private String JavaPath = "C:\\Program Files (x86)\\Java\\jre1.8.0_251\\bin\\java.exe"
+    private String JavaPath = "C:\\Program Files\\Java\\jre1.8.0_251\\bin\\java.exe"
     private String DDManPath= "C:\\Users\\wtammam\\AppData\\Local\\DDMan6\\release\\ddman6.jar"
     private String JavaArchive = "-jar"
     private String JavaMemory = "-Xmx1G"
