0e6c76f6e36be6cfdb45222b3fe2d503f16377f8
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-07-02 20:00:35 +0200

    0207

--- vars/DDManAPI_Function.groovy
+++ vars/DDManAPI_Function.groovy
@@ -15,7 +15,7 @@ def GetData(String DDManPrjVzPk = 'Hallo ich bin') {
     //PK = DDManPrjVzPk.split(' ')[2]
     //println ("${Prj}, ${VZ}, ${PK}")
     //def DDManCommand= "java -jar -Xmx1G C:\\Users\\wtammam\\AppData\\Local\\DDMan6\\release\\ddman6.jar -EXEC INTEGRATION-TEST-B PRJ=M260_M264 PS=19B_Star23_VC10 PK=L07FRG20"
-    def DDManCommand= "cmd java -X >c:\\temp.txt"
+    def DDManCommand= "cmd /c java -X >c:\\temp.txt"
     def DDManexecute= DDManCommand.execute()
     def outputStream = new StringBuffer();
     DDManexecute.waitForProcessOutput(outputStream, System.err)
