d2c0ece624d2b1846192876d495f13b14e8bdffa
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-07-02 19:48:41 +0200

    0207

--- vars/DDManAPI_Function.groovy
+++ vars/DDManAPI_Function.groovy
@@ -7,12 +7,24 @@
 //PrjData PrjConfig = new PrjData()
 
 def GetData(String DDManPrjVzPk = 'Hallo ich bin') {
-    String Prj
-    String VZ
-    String PK
-    Prj = DDManPrjVzPk.split(' ')[0]
-    VZ = DDManPrjVzPk.split(' ')[1]
-    PK = DDManPrjVzPk.split(' ')[2]
-    println ("${Prj}, ${VZ}, ${PK}")
+    //String Prj
+    //String VZ
+    //String PK
+    //Prj = DDManPrjVzPk.split(' ')[0]
+    //VZ = DDManPrjVzPk.split(' ')[1]
+    //PK = DDManPrjVzPk.split(' ')[2]
+    //println ("${Prj}, ${VZ}, ${PK}")
+
+    def DDManexecute= DDManCommand.execute()
+    def outputStream = new StringBuffer();
+    DDManexecute.waitForProcessOutput(outputStream, System.out)
+    //String Prj
+    //String VZ
+    //String PK
+    //Prj = DDManPrjVzPk.split(' ')[0]
+    //VZ = DDManPrjVzPk.split(' ')[1]
+    //PK = DDManPrjVzPk.split(' ')[2]
+    //println ("${Prj}, ${VZ}, ${PK}")
+    println ("${outputStream.toString()}")
 }
 
