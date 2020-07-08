7aff8b183fe5a965dd6a08f931201b1a2e8a435e
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-07-08 19:37:13 +0200

    0807

--- vars/DDManAPI.groovy
+++ vars/DDManAPI.groovy
@@ -102,26 +102,27 @@ def GetData() {
                     else if(DDManJob=="DOKU/DCM/DAISTRUCT/A2L/KGS") {
                         if (Prj != "PT3_Otto_M274") {
                             for (int i = 0; i < DDManJobNew.length - 2; i++) {
-                                if (i ==2 || i ==3) {
+                                if (i ==2 || i == 3) {
                                     DDManCommand = "\"${JavaPath}\" ${JavaArchive} ${JavaMemory} ${DDManNewAPI} ${DDManModus[1]} ${DDManJobNew[i]} -PRJ ${Prj} -SGP ${VZ} -PRG ${PK} -DIR ${WORKINGPLACE}"
                                 } else {
                                     DDManCommand = "\"${JavaPath}\" ${JavaArchive} ${JavaMemory} ${DDManNewAPI} ${DDManModus[1]} ${DDManJobNew[i]} -PRJ ${Prj} -SGP ${VZ} -PRG ${PK} -DIR ${WORKINGPLACE}description"
                                 }
                                 proc = DDManCommand.execute()
-                                sout.append(i)
                                 sout.append(DDManCommand)
+                                sout.append("\n")
                                 proc.waitForProcessOutput(sout, serr)
                             }
                         } else {
                             for (int i = 0; i< DDManJobNew.length; i++) {
-                                if (i != 2 || i != 3 ||i != 4) {
-                                    DDManCommand = "\"${JavaPath}\" ${JavaArchive} ${JavaMemory} ${DDManNewAPI} ${DDManModus[1]} ${DDManJobNew[i]} -PRJ ${Prj} -SGP ${VZ} -PRG ${PK} -DIR ${WORKINGPLACE}description"
-                                } else {
+                                if (i == 2) {
                                     DDManCommand = "\"${JavaPath}\" ${JavaArchive} ${JavaMemory} ${DDManNewAPI} ${DDManModus[1]} ${DDManJobNew[i]} -PRJ ${Prj} -SGP ${VZ} -PRG ${PK} -DIR ${WORKINGPLACE}"
+                                } else {
+                                    DDManCommand = "\"${JavaPath}\" ${JavaArchive} ${JavaMemory} ${DDManNewAPI} ${DDManModus[1]} ${DDManJobNew[i]} -PRJ ${Prj} -SGP ${VZ} -PRG ${PK} -DIR ${WORKINGPLACE}description"
                                 }
-                                if(i != 3 ||i != 4) {
+                                if(i != 3 && i != 4) {
                                     proc = DDManCommand.execute()
                                     sout.append(DDManCommand)
+                                    sout.append("\n")
                                     proc.waitForProcessOutput(sout, serr)
                                 }
                             }
