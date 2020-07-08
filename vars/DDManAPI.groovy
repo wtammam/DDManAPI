2a27c53cf3e4fd4fe1a13c98b8b5eae840821f5d
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-07-08 18:05:54 +0200

    0807

--- vars/DDManAPI.groovy
+++ vars/DDManAPI.groovy
@@ -100,7 +100,7 @@ def GetData() {
                         proc.waitForProcessOutput(sout, serr)
                     }
                     else if(DDManJob=="DOKU/DCM/DAISTRUCT/A2L/KGS") {
-                        if (Prj != "PT3_Otto") {
+                        if (Prj != "PT3_Otto_M274") {
                             for (int i = 0; i < DDManJobNew.length - 2; i++) {
                                 if (i != 2 || i != 3) {
                                     DDManCommand = "\"${JavaPath}\" ${JavaArchive} ${JavaMemory} ${DDManNewAPI} ${DDManModus[1]} ${DDManJobNew[i]} -PRJ ${Prj} -SGP ${VZ} -PRG ${PK} -DIR ${WORKINGPLACE}\\description"
