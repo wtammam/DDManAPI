2b8b992ffd966a6dcaa598883db659bf19a31e3d
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-07-16 20:29:05 +0200

    Implementierung Auto

--- vars/DDManAPI.groovy
+++ vars/DDManAPI.groovy
@@ -135,7 +135,7 @@ def GetData() {
                         xyz = ConsoleOutputCheck("${OutAndError}", ErrorList as String[])
                         if (xyz == true) {
                             status = "Error"
-                            throw new Exception ("some error message");
+                            //throw new Exception ("some error message");
                         } else
                             status = "no Error"
                     }
