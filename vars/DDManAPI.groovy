d5086d2102c30852cd4c10ff08d666a4f31750ec
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-07-16 17:37:37 +0200

    Implementierung Auto

--- vars/DDManAPI.groovy
+++ vars/DDManAPI.groovy
@@ -120,7 +120,7 @@ def GetData() {
                 break
             case "AUTO":
                 //SetDDManAPI("NEW")
-                try {
+               /* try {
                     OutAndError = NewDDManAPI(Prj, VZ, PK, DDManJobNew)
                     ErrorList = ["NO_CONNECTION_TO_SERVER", "ERROR:"]
                     //def ErrorList= ["NO_CONNECTION_TO_SERVER","ERROR:","no connection to","SCHWERWIEGEND:"] as String[]
@@ -128,18 +128,18 @@ def GetData() {
 
                     if (Errorfound == true) {
                         OutAndError[1].append("\n")
-                        OutAndError[1].append("-->Error with New API has occurred")
+                        OutAndError[1].append("-->Error with New API has occurred")*/
                         OutAndError += OldDDManAPI(Prj, VZ, PK, DDManJobOld)
                         ErrorList = ["connection", "SCHWERWIEGEND:"]
-                        xyz = ConsoleOutputCheck("${OutAndError}", ErrorList as String[])
-                        if (xyz == true) {
+                Errorfound = ConsoleOutputCheck("${OutAndError}", ErrorList as String[])
+                       /* if (xyz == true) {
                             status = "Error"
                         } else
                             status = "no Error"
                     }
                 }catch(IOException){
                     status = "schwerer Error"
-                }
+                }*/
                break
             default:
                 break
@@ -173,7 +173,7 @@ def GetData() {
         sout=OutAndError[0]
         serr=OutAndError[1]
         //return ("$sout, $serr")
-        return ("$OutAndError, $Errorfound, $status")
+        return ("$sout, $Errorfound, $status")
         //return ("$Errorfound")
        // } catch(Exception e) {
         //return("Exception: ${e}")
