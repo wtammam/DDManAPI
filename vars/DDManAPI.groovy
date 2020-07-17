f1eb7d7eef540831736efbaed13328e8c9dd58b6
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-07-17 16:31:17 +0200

    Auto test

--- vars/DDManAPI.groovy
+++ vars/DDManAPI.groovy
@@ -3,9 +3,9 @@
 //package vars
 class DDManAPI {
 
-    //private String JavaPath = "C:\\Program Files (x86)\\Java\\jre1.8.0_251\\bin\\java.exe"
+    private String JavaPath = "C:\\Program Files (x86)\\Java\\jre1.8.0_261\\bin\\java.exe"
     private String JavaPath = "java"
-    //private String JavaPath = "C:\\Program Files\\Java\\jre1.8.0_251\\bin\\java.exe"
+    //private String JavaPath = "C:\\Program Files\\Java\\jre1.8.0_261\\bin\\java.exe"
     private String DDManPath= "C:\\Users\\wtammam\\AppData\\Local\\DDMan6\\release"
     private String JavaArchive = "-jar"
     private String JavaMemory = "-Xmx1G"
