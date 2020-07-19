9c8c864089e066d4fcffd44b2ac92d8cb5686243
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-07-20 01:06:27 +0200

    Auto test

--- vars/DDManAPI.groovy
+++ vars/DDManAPI.groovy
@@ -183,7 +183,7 @@ def GetData() {
         //sfound=OutAndError[2]
         //return ("$sout, $serr")
        // return ("$OutAndError, $sfound")
-        return ("$OutAndError, $sfound")
+        return (OutAndError.toString())
         //return ("$Errorfound")
        // } catch(Exception e) {
         //return("Exception: ${e}")
