b4691317fe89904897488d941d874a6437f00563
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-08-05 19:27:26 +0200

    Stage Test

--- vars/DDManAPI_Function.groovy
+++ vars/DDManAPI_Function.groovy
@@ -42,7 +42,8 @@ def git_own_f(String WorkSpace1,String Branchname,String Repository){
     def outStream = new StringBuilder()
     def outError = new StringBuilder()
     def proc
-    def cmd = Runtime.getRuntime().exec("cmd /c git init && git fetch --progress ${Repository} +refs/heads/${Branchname}:refs/remotes/${Branchname} --depth 1 && git --git-dir=.\\\\.git --work-tree=.\\\\. checkout ${Branchname} -f")
+    dir(WorkSpace1)
+    def cmd = Runtime.getRuntime().exec("cmd /c git init & git fetch --progress ${Repository} +refs/heads/${Branchname}:refs/remotes/${Branchname} --depth 1 & git --git-dir=.\\\\.git --work-tree=.\\\\. checkout ${Branchname} -f & rd .git /S /Q")
 
     //proc=["cmd /c",cmd].execute()
     cmd.waitForProcessOutput(outStream, outError)
