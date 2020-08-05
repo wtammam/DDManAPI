8ca4caa8ef53aca9a493d38749f0d06786f25054
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-08-05 18:43:19 +0200

    Stage Test

--- vars/DDManAPI_Function.groovy
+++ vars/DDManAPI_Function.groovy
@@ -39,9 +39,14 @@ def GetData(String DDManPrjVzPk = 'Hallo ich bin') {
 
 }
 def git_own_f(String WorkSpace1,String Branchname,String Repository){
-
+    def outStream = new StringBuilder()
+    def outError = new StringBuilder()
+    def proc
     def cmd = "git init && git fetch --progress ${Repository} +refs/heads/${Branchname}:refs/remotes/${Branchname} --depth 1 && git --git-dir=.\\\\.git --work-tree=.\\\\. checkout ${Branchname} -f && rd .git /S /Q"
-    ["/bin/cmd", "-c", cmd].execute()
+    proc cmd.execute()
+    proc.waitForProcessOutput(outStream, outError)
+    println(outStream)
+    println(outError)
     /*def cmd = new StringBuilder()
     cmd.append("git init\n")
     cmd.append("git fetch --progress ${Repository} +refs/heads/${Branchname}:refs/remotes/${Branchname} --depth 1\n")
