60d75988b0e86983519a4e2782c784b15677e0f3
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-08-05 14:07:02 +0200

    Stage Test

--- vars/DDManAPI_Function.groovy
+++ vars/DDManAPI_Function.groovy
@@ -43,21 +43,19 @@ def git_own_f(String WorkSpace1,String Branchname,String Repository){
     def result=0
     def abbruch=0
 
-  //  try{
+    try{
         dir(WorkSpace1){
             output=bat ([label:"git checkout/s ${Repository}/s ${Branchname}/s --depth 1", returnStdout:true, script:
                     """
 					git init
-					git fetch --progress ${Repository} +refs/heads/${Branchname}:refs/remotes/${Branchname} --depth 1 
-					git --git-dir=.\\.git --work-tree=.\\. checkout ${Branchname} -f 
-					rd .git /S /Q
+
 				"""])
         }
-   /*}
+    }
     catch(IOException){
         result =1
         abbruch =-1
-    }*/
+    }
 
 	println output
 	println result
