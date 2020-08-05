aa8759e78518c185a4a8e5d3ef0a12be9424e4fb
Wajih Tammam <wajih.tammam@itk-engineering.de>
2020-08-05 14:23:30 +0200

    Stage Test

--- vars/DDManAPI_Function.groovy
+++ vars/DDManAPI_Function.groovy
@@ -49,7 +49,7 @@ def git_own_f(String WorkSpace1,String Branchname,String Repository){
 
     try{
         dir(WorkSpace1){
-            output=bat (label:"git checkout/s ${Repository}/s ${Branchname}/s --depth 1", returnStdout:true, script:"${cmd.toString()}"
+            output=bat (label:"git checkout/s ${Repository}/s ${Branchname}/s --depth 1", returnStdout:true, script:"${cmd}"
 
             )
         }
